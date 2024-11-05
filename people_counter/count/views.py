93834f23ea5a3aca98ebc831d437fe946268f1ca
daichi <gangdazhi955@gmail.com>
2024-11-05 19:00:30 +0900

    url

--- people_counter/count/views.py
+++ people_counter/count/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
 from .models import PeopleCounter
+from django.shortcuts import redirect
+
+def redirect_to_get_count(request):
+    return redirect('get_count')  # 名前付きURLを使って`/counter/get-count/` へリダイレクト
+
 
 def UpdateCountView(request, current_count):
     PeopleCounter.objects.update_or_create(id=1, defaults={'count': current_count})
